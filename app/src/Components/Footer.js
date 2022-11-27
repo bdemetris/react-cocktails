a85a2fa6e51f4a1ddca9fc92fd98a8ec4959d698
brett-demetris <brett.demetris@coinbase.com>
2022-11-27 11:09:06 -0800

    update footer

--- app/src/Components/Footer.js
+++ app/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <>
-            <p className="footer">Copyright @Suraj</p>
+            <p className="footer">🤣</p>
         </>
     )
 }
